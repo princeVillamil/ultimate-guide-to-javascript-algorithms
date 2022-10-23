e11b4167d4902985da1a5fe74352562598ff5bba
princeVillamil <jeffreyvillamilwork@gmail.com>
2022-10-22 22:33:18 -0600

    more done

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -6,15 +6,45 @@
 
 function mergeArrays(...arrays) {
 
-    let jointArray = []
-    
-    arrays.forEach(array => {
-        jointArray = [...jointArray, ...array]
-    });
+    // Example
+        // let jointArray = []
+        // arrays.forEach(array => {
+        //     jointArray = [...jointArray, ...array]
+        // });
+        // return [...new Set([...jointArray])]
 
-    return [...new Set([...jointArray])]
+    // let array = Array.from(arrays) // method that creates a new array instance from an array-like or iterable object
+    // console.log(array)
 
-    
+    // Combining the arrays
+        // let jointArray = []
+        // arrays.forEach(array=> jointArray = [...jointArray, ...array])
+
+    // Using sets
+        // a SET in js is an object used to store a collection of values of any type
+        // let jointArray = []
+        // arrays.forEach(arr=> jointArray = [...jointArray, ...arr] )
+        // return [...new Set([...jointArray])] // This will eliminates all dup
+
+    // Using Array.from
+        // let jointArray = []
+        // arrays.forEach(arr=>jointArray = [...jointArray, ...arr])
+        // return Array.from(new Set([...jointArray]))
+
+    // Using filter FASTEST <-----------------------------------
+        // let jointArray = []
+        // arrays.forEach(arr=> jointArray = [...jointArray, ...arr] )
+        // const uniqueArray = jointArray.filter((x, i)=> jointArray.indexOf(x) === x )
+        // return uniqueArray
+
+    // Using reduce
+        // let jointArray = []
+        // arrays.forEach(arr=> jointArray = [...jointArray, arr])
+        // const uniqueArray = jointArray.reduce((acc, curr)=>{
+        //     if(acc.includes(curr)) return acc
+        //     else return [...acc, curr]
+        // }, [])
+        // return uniqueArray
 }
 
 
